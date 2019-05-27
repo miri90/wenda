94ba4e01b8261c5fcc508fe370a09fe21d9ebba3
llr <530195725@qq.com>
2019-05-27 18:43:10 +0800

    拖动事件

--- TigerAndDog/src/com/njupt/tigerdog/model/TestModel.java
+++
@@ -1,9 +0,0 @@
-package com.njupt.tigerdog.model;
-
-import com.njupt.tigerdog.seizeTigerChessGame.SeizeTigerChess;
-
-public class TestModel {
-	public static void main(String[] args) {
-		new SeizeTigerChess("捕虎棋");
-	}
-}
